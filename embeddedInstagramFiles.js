b4975ad5b577a7731f43876dd2756c4e4de195cc
Adityakhari <79889587+Adityakhari@users.noreply.github.com>
2026-03-09 19:30:01 +0530

    Add files via upload

---
+++ embeddedInstagramFiles.js
@@ -0,0 +1,5 @@
+import chatPart1 from "@/data/chat.json";
+import chatPart2 from "@/data/message_2.json";
+import chatPart3 from "@/data/message_3.json";
+
+export const embeddedInstagramFiles = [chatPart1, chatPart2, chatPart3];
